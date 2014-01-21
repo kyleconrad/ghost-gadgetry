eda71ef1951a0e8a72e1ddc96b91aa754a04f4b2
Kyle Conrad <kyle@kyleconrad.com>
2014-01-21 10:27:22 -0500

    Fix JS directory

--- SASS/config.rb
+++ SASS/config.rb
@@ -5,7 +5,7 @@ http_path = "/"
 css_dir = "../css"
 sass_dir = "../sass"
 images_dir = "images"
-javascripts_dir = "fonts"
+javascripts_dir = "../JS"
 fonts_dir = "../FONTS"
 
 # You can select your preferred output style here (can be overridden via the command line):
